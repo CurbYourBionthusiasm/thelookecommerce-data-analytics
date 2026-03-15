78ff2916e3810768621cafe785c172bb6e64391f
Tugdo Enkhbaatar <enkh.tug@gmail.com>
2026-03-15 22:52:29 +0100

    Add SQL query for AOV distribution analysis

---
+++ sql/03_aov_distribution.sql
@@ -0,0 +1,14 @@
+--=============================================================================
+--AOV Distribution Analysis
+--Dataset: bigquery-public-data.thelook_ecommerce
+--Purpose: Understand the distribution of order values and identify 
+--potential skewness or high-value outliers
+--Grain = 1 row = 1 order
+--=============================================================================
+
+SELECT 
+  order_id, 
+  SUM(sale_price) AS revenue_per_order, 
+FROM `bigquery-public-data.thelook_ecommerce.order_items`
+WHERE status = "Complete"
+GROUP BY order_id;

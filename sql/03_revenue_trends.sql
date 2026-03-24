af183c82dc57cf31d193406b38d618bcc82478e8
Tugdo Enkhbaatar <enkh.tug@gmail.com>
2026-03-24 16:44:13 +0100

    Debugged and removed small errors

--- sql/03_revenue_trends.sql
+++ sql/03_revenue_trends.sql
@@ -23,40 +23,33 @@
 =============================================================================
 
 WITH order_revenue AS (
-SELECT 
-  order_id,
-  user_id, 
-  SUM(sale_price) AS revenue_per_order, 
-  DATE_TRUNC(DATE(created_at),MONTH) AS month
-FROM `bigquery-public-data.thelook_ecommerce.order_items`
-WHERE status = "Complete"
-GROUP BY DATE_TRUNC(DATE(created_at),MONTH), order_id
-ORDER BY month
+  SELECT 
+    order_id,
+    user_id,
+    DATE_TRUNC(DATE(created_at), MONTH) AS month,
+    SUM(sale_price) AS revenue_per_order
+  FROM `bigquery-public-data.thelook_ecommerce.order_items`
+  WHERE status = "Complete"
+  GROUP BY order_id, user_id, month
 ),
 
 monthly_revenue AS (
-
-SELECT
-  month,
-  COUNT(DISTINCT(order_id)) AS total_orders,
-  COUNT(DISTINCT(user_id)) AS total_users
-  SUM(revenue_per_order) AS total_revenue,
-
-FROM order_revenue
-GROUP BY month
+  SELECT
+    month,
+    COUNT(DISTINCT order_id) AS total_orders,
+    COUNT(DISTINCT user_id) AS total_users,
+    SUM(revenue_per_order) AS total_revenue
+  FROM order_revenue
+  GROUP BY month
 )
 
 SELECT
-    month,
-    total_orders,
-    total_users,
-    total_revenue,
-
-    ROUND(total_revenue / total_orders,2) AS AOV,
-    ROUND(total_revenue / total_users,2) AS revenue_per_user,
-    ROUND(total_orders / total_users,2) AS orders_per_user
-
+  month,
+  total_orders,
+  total_users,
+  total_revenue,
+  ROUND(total_revenue / total_orders, 2) AS AOV,
+  ROUND(total_revenue / total_users, 2) AS revenue_per_user,
+  ROUND(total_orders / total_users, 2) AS orders_per_user
 FROM monthly_revenue
 ORDER BY month;
-
-;
